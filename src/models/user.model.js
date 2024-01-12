a9782884b930d8cbba8b8efa3503c747562f584f
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-01-12 22:19:58 +0530

    add register controller and fixed bugs

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -65,7 +65,7 @@ userSchema.pre("save", async function(next) {
 
     if (!this.isModified("password")) return next();
     
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -82,7 +82,7 @@ userSchema.methods.generateAccessToken = function() {
     },
         process.env.ACCESS_TOKEN_SECRET,
        {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
        }
     ) 
 }
